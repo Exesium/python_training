7ef405f183534f9429eb9df5b905d15ce282f57f
Иван Фадеев <zetcuowne@gmail.com>
2016-07-24 20:59:35 +0300

    Урок 2.4
    Продолжение

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,19 +1,10 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
 from model.contact import Contact
 
 # Вынесем отдельный объект
 new_contact = Contact(firstname="Jon", lastname="Exesium", nickname="Happy", title="Engineer",
                              company="ATOL", address="Moscow", mobile="+7 985 966 44 24",
                              work="+7 (495) 730-74-20", email="i.petrov@gmail.com")
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
