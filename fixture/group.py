2029ea446ca0558502d4ae80f4452b19a39d896c
Иван Фадеев <zetcuowne@gmail.com>
2016-07-24 21:17:11 +0300

    Редактирование группы

--- fixture/group.py
+++ fixture/group.py
@@ -30,10 +30,32 @@ class GroupHelper:
         # submit new group
         wd.find_element_by_name("submit").click()
 
+    def edit_1st_group(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # checking 1st element
+        wd.find_element_by_name("selected[]").click()
+        # edit-button clicking
+        wd.find_element_by_name("edit").click()
+        # editing forms
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("group_footer").click()
+        # submit changes
+        wd.find_element_by_name("update").click()
+
     def del_1st_group(self):
         wd = self.app.wd
         self.open_group_page()
         # checking 1st element
         wd.find_element_by_name("selected[]").click()
-        # Delete-button clicking
+        # delete-button clicking
         wd.find_element_by_name("delete").click()
