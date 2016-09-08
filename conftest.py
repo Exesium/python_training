e2bfbae94e82ed07a5ad9c4e8527dbda4940ca01
Иван Фадеев <zetcuowne@gmail.com>
2016-09-08 21:51:28 +0300

    Упр. 17

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+import os.path
 import pytest
 from fixture.application import Application
 
@@ -13,8 +14,9 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config:
-            target = json.load(config)
+        conf = os.path.join(os.path.dirname(os.path.abspath(__file__)), (request.config.getoption("--target")))
+        with open(conf) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, baseurl=target['baseurl'])
     fixture.session.ensure_login(username=target['username'], password=target['password'])
