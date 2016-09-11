a3256e9a521b09b1d3f89aa8df8570a3ac705acd
Иван Фадеев <zetcuowne@gmail.com>
2016-09-11 17:39:09 +0300

    Упр. 19
    Загрузка тестовых данных из файлов при добавлении групп и контактов, а
    также при редактировании групп.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,8 +3,8 @@ from model.group import Group
 from random import randrange
 
 
-def test_edit_group_name_by_index(app):
-    group = Group(name="CHANGED_GROUP!")
+def test_edit_group_name_by_index(app, json_groups):
+    group = json_groups
     if app.group.count() == 0:
         app.group.create(Group(name="When_there_is_no_GROUP"))
     old_groups = app.group.get_group_list()
@@ -15,15 +15,3 @@ def test_edit_group_name_by_index(app):
     new_groups = app.group.get_group_list()
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-"""
-def test_edit_1st_group_header(app):
-    group = Group(header="CHANGED_HEADER!")
-    if app.group.count() == 0:
-        app.group.create(Group(name="When_there_is_no_GROUP"))
-    old_groups = app.group.get_group_list()
-    app.group.edit_1st(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
-"""
