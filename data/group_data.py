2ab936b5808d53c37092fed30f23d6473c4cb0b0
Иван Фадеев <zetcuowne@gmail.com>
2016-09-11 16:05:08 +0300

    6.4

--- data/group_data.py
+++ data/group_data.py
@@ -2,8 +2,10 @@ from model.group import Group
 import random
 import string
 
-constant = [Group(name='name1', header='header1', footer='footer1')] +\
-           [Group(name='name2', header='header2', footer='footer2')]
+constant = [
+    Group(name='name1', header='header1', footer='footer1'),
+    Group(name='name2', header='header2', footer='footer2')
+    ]
 
 
 def random_string(prefix, maxlen):
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.group import Group
-from data.group_data import constant as testdata
+from data.group_data import testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
